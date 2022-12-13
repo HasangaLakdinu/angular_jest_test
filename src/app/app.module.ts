c4b588777cefb1233316f2bf94f4cf0d608fe9eb
Hasanga Karunadasa <hasanga@corzent.com>
2022-12-13 11:33:36 +0530

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,24 @@
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MatchersComponent } from './matchers/matchers.component';
+import { DataComponent } from './data/data.component';
+import { AsynchronousComponent } from './asynchronous/asynchronous.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MatchersComponent,
+    DataComponent,
+    AsynchronousComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
